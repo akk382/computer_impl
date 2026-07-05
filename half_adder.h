a33b26e7d75bb644a391b15560df7c9931586ba7
akk382 <arunyoyo00@gmail.com>
2026-07-04 23:13:45 -0500

    added half-adder

---
+++ half_adder.h
@@ -0,0 +1,12 @@
+#include "wire.h"
+
+struct half_adder {
+  wire_t* in1;
+  wire_t* in2;
+  wire_t* sum;  // XOR(in1, in2)
+  wire_t* carry; // AND(in1, in2)
+};
+
+typedef struct half_adder half_adder;
+
+void ha_update(half_adder*);
